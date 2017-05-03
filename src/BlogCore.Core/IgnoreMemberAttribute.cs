b956e74c489d83a610f9a9ffc604bff4094fa82a
thangchung <thangchung@ymail.com>
2017-05-03 17:40:50 +0700

    #1 add IgnoreMember attr back :(

---
+++ src/BlogCore.Core/IgnoreMemberAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BlogCore.Core
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
