05d3bd83fd8d0be34db8bdd794a70300df7e246a
thangchung <thangchung@ymail.com>
2017-10-15 14:52:23 +0700

    update deploy scripts

--- builds/start.ps1
+++ builds/start.ps1
@@ -1,3 +1,2 @@
-Start-Process Powershell.exe -Argumentlist "-NoExit -file start_access_control_host.ps1" -Verb runAs
-Start-Process Powershell.exe -Argumentlist "-NoExit -file start_api_gateway_host.ps1" -Verb runAs
+Start-Process Powershell.exe -Argumentlist "-NoExit -file start_webapp_host.ps1" -Verb runAs
 Start-Process Powershell.exe -Argumentlist "-NoExit -file start_web.ps1" -Verb runAs
