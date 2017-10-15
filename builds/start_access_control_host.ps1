fa3f66cdf452881954e81a5c9b3c7024455c3f64
thangchung <thangchung@ymail.com>
2017-10-15 14:55:48 +0700

    deleted the Auth host script

--- builds/start_access_control_host.ps1
+++
@@ -1,9 +0,0 @@
-$servicePath = "$PSScriptRoot\..\src\Hosts\BlogCore.IdentityServer"
-
-sl $servicePath
-# & "C:\Program Files\dotnet\dotnet.exe" restore
-# & "C:\Program Files\dotnet\dotnet.exe" build
-& "C:\Program Files\dotnet\dotnet.exe" run
-
-$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
-$HOST.UI.RawUI.Flushinputbuffer()
