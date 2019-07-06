09ce586024431757122a4207444c2bdd42306b50
Thang Chung <thangchung@ymail.com>
2019-07-06 18:09:18 +0700

    add validation

---
+++ src/BlogCore.Shared/v1/Validators/CreateBlogRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using BlogCore.Shared.v1.Blog;
+using FluentValidation;
+
+namespace BlogCore.Shared.v1.Validators
+{
+    public class CreateBlogRequestValidator : AbstractValidator<CreateBlogRequest>
+    {
+        public CreateBlogRequestValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Title could not be null or empty.");
+        }
+    }
+}
